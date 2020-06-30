7ef84c0bdd73f6d804fb2f40be0e03e2c76086b0
tom2u <tbrodhead@2u.com>
2020-06-30 15:17:31 -0500

    20.2.6

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -13,4 +13,17 @@ describe("Nav component", () => {
     const { asFragment } = render(<Nav />);
     expect(asFragment()).toMatchSnapshot();
   });
+  describe("emoji is visible", () => {
+    it("inserts emoji into the h2", () => {
+      const { getByLabelText } = render(<Nav />);
+      expect(getByLabelText("camera")).toHaveTextContent("📸");
+    });
+  });
+  describe("links are visible", () => {
+    it("inserts text into the links", () => {
+      const { getByTestId } = render(<Nav />);
+      expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+      expect(getByTestId("about")).toHaveTextContent("About me");
+    });
+  });
 });
